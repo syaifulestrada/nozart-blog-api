33021c32c2439af1f81e72471e4c1af7bf8cd19e
syaifulestrada <syaifultheminister@gmail.com>
2026-02-20 09:23:22 +0700

    install express js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+import express from "express";
+
+const app = express();
+const port = 300;
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
