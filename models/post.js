2323aed8f9210f0bb8584a9daa358cc87c905561
syaifulestrada <syaifultheminister@gmail.com>
2026-02-21 11:38:13 +0700

    feat: buat validasi untuk handle jika id post tidak ditemukan saat update

--- models/post.js
+++ models/post.js
@@ -38,6 +38,12 @@ async function updateData(payload, id) {
             id,
         ]);
 
+        if (result.affectedRows === 0) {
+            throw Object.assign(new Error("post tidak ditemukan."), {
+                status: 404,
+            });
+        }
+
         return result.affectedRows;
     } catch (error) {
         console.error(error);
