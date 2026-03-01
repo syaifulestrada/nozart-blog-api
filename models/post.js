a5a51ea2bc53d67f5bf851cd8f542040cea686e5
syaifulestrada <syaifultheminister@gmail.com>
2026-03-01 12:49:01 +0700

    feat: kirim response cover post ke detail post

--- models/post.js
+++ models/post.js
@@ -17,9 +17,13 @@ async function selectData() {
 
 async function detailData(postId) {
     try {
-        const sqlStatement = `SELECT posts.id, posts.title, posts.content, GROUP_CONCAT(categories.name SEPARATOR ", ") AS categories, posts.created_at, posts.updated_at FROM post_categories LEFT JOIN posts ON post_categories.post_id = posts.id LEFT JOIN categories ON post_categories.category_id = categories.id WHERE posts.id = ? GROUP BY posts.id`;
+        const sqlStatement = `SELECT posts.id, posts.title, posts.content, posts.cover, GROUP_CONCAT(categories.name SEPARATOR ", ") AS categories, posts.created_at, posts.updated_at FROM post_categories LEFT JOIN posts ON post_categories.post_id = posts.id LEFT JOIN categories ON post_categories.category_id = categories.id WHERE posts.id = ? GROUP BY posts.id`;
 
         const [rows] = await pool.query(sqlStatement, [postId]);
+
+        rows.forEach((row) => {
+            row.cover = row.cover ? `http://localhost:3000/${row.cover}` : null;
+        });
 
         if (rows.length === 0) {
             throw Object.assign(new Error("post tidak ditemukan."), {
